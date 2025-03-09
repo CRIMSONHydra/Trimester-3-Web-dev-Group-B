13410d64818de6f2b2084b89a99cf8692167d0bb
CRIMSONHydra <53392400+CRIMSONHydra@users.noreply.github.com>
2025-03-09 15:12:22 +0530

    imdb initial

---
+++ imdb-clone/src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <nav>
+      <a>Logo</a>
+      <a className='active'>Home</a>
+      <a>Watch List</a>
+      <a>Recommendations</a>
+    </nav>
+  )
+}
+
+export default Navbar
