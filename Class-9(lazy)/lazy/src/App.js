c6457e87d905e28914085306aa74ab0b9b54ce23
mrinal1224 <iammrinalkc@gmail.com>
2025-04-07 11:27:35 +0530

    let;s talk lazy

--- Class-9(lazy)/lazy/src/App.js
+++ Class-9(lazy)/lazy/src/App.js
@@ -1,11 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-import Movies from './components/Movies';
+import logo from "./logo.svg";
+import "./App.css";
+import Movies from "./components/Movies";
+import Navbar from "./components/Navbar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Home from "./components/Home";
 
 function App() {
   return (
     <div className="App">
-      <Movies/>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/home" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
