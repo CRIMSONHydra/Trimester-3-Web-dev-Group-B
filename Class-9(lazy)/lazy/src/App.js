fb8631baa1bf4b1a075295c01340d5cd0cfeb99c
mrinal1224 <iammrinalkc@gmail.com>
2025-04-08 13:55:09 +0530

    push

--- Class-9(lazy)/lazy/src/App.js
+++ Class-9(lazy)/lazy/src/App.js
@@ -1,23 +1,32 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Movies from "./components/Movies";
+import { lazy , Suspense} from "react";
 import Navbar from "./components/Navbar";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import About from "./components/About";
-import Contact from "./components/Contact";
-import Home from "./components/Home";
+const Home = lazy(()=> import('./components/Home'))
+
+const About = lazy(()=> import('./components/About'))
+
+const Contact = lazy(()=> import('./components/Contact'))
+
+
+
 
 function App() {
   return (
     <div className="App">
+     <Suspense fallback={<h1>Loadingg..</h1>}>
       <BrowserRouter>
         <Navbar />
         <Routes>
-          <Route path="/home" element={<Home />} />
+    
+          <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
         </Routes>
       </BrowserRouter>
+      </Suspense>
     </div>
   );
 }
