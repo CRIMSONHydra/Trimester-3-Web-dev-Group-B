23756c2da88fff2345fc98325f0e2686ef9f885c
mrinal1224 <iammrinalkc@gmail.com>
2025-04-09 11:29:37 +0530

    push

--- Class-10(Hooks)/vite-project/src/components/Memo.jsx
+++ Class-10(Hooks)/vite-project/src/components/Memo.jsx
@@ -1,9 +1,11 @@
-import React, { useMemo, useState } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 
 function Memo() {
   const [count, setCount] = useState(0);
   const [number, setNumber] = useState(0);
 
+  let doubleValue2=0;
+  
   function doubleValue(num) {
     console.log(`Running for the num ${num}`);
     for (let i = 0; i < 1000000000; i++) {}
@@ -12,7 +14,11 @@ function Memo() {
 
 //    const doubledValue = doubleValue(number);// 2
 
- const doubledValue = useMemo(()=>doubleValue(number), [number])
+//  const doubledValue = useMemo(()=>doubleValue(number), [number])
+
+useEffect(()=>{
+    doubleValue2 = doubleValue(number)
+} , [number])
 
   return (
     <div>
@@ -25,7 +31,7 @@ function Memo() {
         onChange={(e) => setNumber(e.target.value)}
       ></input>
 
-      <h3>Doubled Value : {doubledValue}</h3>
+      <h3>Doubled Value : {doubleValue2}</h3>
     </div>
   );
 }
