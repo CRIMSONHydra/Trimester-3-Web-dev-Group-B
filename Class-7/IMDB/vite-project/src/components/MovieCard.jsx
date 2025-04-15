13475fb9b28f610f049571bbb9ac4abf9d5f939d
CRIMSONHydra <53392400+CRIMSONHydra@users.noreply.github.com>
2025-04-15 15:10:39 +0530

    comit

--- Class-7/IMDB/vite-project/src/components/MovieCard.jsx
+++ Class-7/IMDB/vite-project/src/components/MovieCard.jsx
@@ -1,20 +1,21 @@
 import React from "react";
 
-function MovieCard({movieObj , handleAddTowatchlist}) {
+function MovieCard({ movieObj, handleAddTowatchlist }) {
   return (
     <div
       className="h-[40vh] w-[200px] bg-cover flex items-end rounded-lg hover:scale-110 duration-300"
       style={{
-        backgroundImage:
-          `url(https://image.tmdb.org/t/p/original/${movieObj.poster_path})`,
+        backgroundImage: `url(https://image.tmdb.org/t/p/original/${movieObj.poster_path})`,
       }}
     >
-       <div onClick={()=>handleAddTowatchlist(movieObj)}  className="m-4 flex justify-center h-8 w-8 items-center rounded-lg bg-gray-900/60">
-       &#128525;
-         
-        </div>
+      <div
+        onClick={() => handleAddTowatchlist(movieObj)}
+        className="m-4 flex justify-center h-8 w-8 items-center rounded-lg bg-gray-900/60"
+      >
+        &#128525;
+      </div>
       <div className="text-white w-full text-center text-xl p-2 bg-gray-900/70 rounded-lg">
-       {movieObj.title}
+        {movieObj.title}
       </div>
     </div>
   );
--- imdb-clone/src/components/Navbar.jsx
+++ imdb-clone/src/components/Navbar.jsx
@@ -1,13 +1,22 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 function Navbar() {
   return (
-    <nav>
-      <a>Logo</a>
-      <a className='active'>Home</a>
-      <a>Watch List</a>
-      <a>Recommendations</a>
-    </nav>
+    <div className="flex space-x-8 items-center pl-3 py-4">
+      <Link to='/'><img className="w-[60px]"/></Link>
+
+      <Link to="/"  className="text-blue-500 text-3xl font-bold">
+        Movies
+      </Link>
+
+      <Link to="/watchlist" className="text-blue-500 text-3xl font-bold">
+        Watchlist
+      </Link>
+      <Link to="/recommend" className="text-blue-500 text-3xl font-bold">
+        Movie Recommendations AI
+      </Link>
+    </div>
   )
 }
 
